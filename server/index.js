77b421a8de8fad2524f2e89c84a71e43ab2d0b55
Oleksandr Leonov <oleksandr.leonov.work@gmail.com>
2024-03-11 17:43:20 +0200

    vercel static

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('static'));
+app.use(express.static(__dirname + "/static"));
 app.use(fileUpload({}));
 app.use('/api', router);
 
